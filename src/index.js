e9b6558e98fe9e93e23a942612a2b94430308d2c
Vincent <vincentliu99999@users.noreply.github.com>
2019-05-09 00:23:36 +0800

    change port to 3000

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-var http = require("http");
+const http = require("http");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello Node!\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
